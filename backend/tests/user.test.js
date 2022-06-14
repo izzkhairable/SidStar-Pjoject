7e603f06e267f6d43f9ee26e8bf25a1be13a4126
izzkhairable <60332263+izzkhairable@users.noreply.github.com>
2022-06-15 01:21:23 +0800

    Backend testing for routes folder full working

--- backend/tests/user.test.js
+++ backend/tests/user.test.js
@@ -5,12 +5,12 @@ const supertest = require('supertest');
 const requestWithSupertest = supertest(server);
 
 
-// describe('User Endpoints', () => {
+describe('User Endpoints', () => {
 
-//     it('GET /users should show all users', async () => {
-//         const res = await requestWithSupertest.get('/users');
-//         expect(res.status).toEqual(200);
-//         expect(res.type).toEqual(expect.stringContaining('json'));
-//         expect(res.body).toHaveProperty('users')
-//     });
-// });
+    it('GET /users should show all users', async () => {
+        const res = await requestWithSupertest.get('/users');
+        expect(res.status).toEqual(200);
+        expect(res.type).toEqual(expect.stringContaining('json'));
+        expect(res.body).toHaveProperty('users')
+    });
+});
