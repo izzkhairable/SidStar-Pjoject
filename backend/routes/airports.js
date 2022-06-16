b22a706e2d326bff41cb8df8a5337af939afb68c
izzkhairable <60332263+izzkhairable@users.noreply.github.com>
2022-06-17 02:55:55 +0800

    Added env api key to sync with secrets

--- backend/routes/airports.js
+++ backend/routes/airports.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var router = express.Router();
 var fetch = require('node-fetch');
+require('dotenv').config({ path: '../../.env' });
+
 /* GET users listing. */
 router.get('/', async function (req, res, next) {
-  // res.send('respond with a resource');
     const response = await fetch('https://open-atms.airlab.aero/api/v1/airac/airports', {
     method: 'get',
-    headers: {'api-key': ''}
+    headers: {'api-key': process.env.API_KEY }
   });
   const data = await response.json();
   res.send(data)
