b95188b225968abd0dd3b28977d55ade2ddf3da3
izzkhairable <60332263+izzkhairable@users.noreply.github.com>
2022-06-18 03:01:50 +0800

    Display Map

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,15 +3,19 @@ import MainDrawer from './components/MainDrawer'
 import Box from '@mui/material/Box';
 import DrawerHeader from './components/DrawerHeader';
 import MainTable from './components/MainTable';
+import Map from './components/Map'
+
 function App(props) {
+
 	return (<>
-	    <Box sx={{ display: 'flex' }}>
-		<MainDrawer props={props}></MainDrawer>
-	     <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-        	<DrawerHeader/>
-			<MainTable></MainTable>
-      		</Box>
-    	</Box>
+		<Box sx={{ display: 'flex' }}>
+			<MainDrawer props={props}></MainDrawer>
+			<Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+				<DrawerHeader />
+				<Map/>
+				<MainTable></MainTable>
+			</Box>
+		</Box>
 	</>)
 }
 
