8d9f964b1d43c8bb1ac66958ef860a7b0b6c6d43
izzkhairable <60332263+izzkhairable@users.noreply.github.com>
2022-06-20 07:41:36 +0800

    Backend Host URL Updated

--- frontend/src/get/airports.js
+++ frontend/src/get/airports.js
@@ -1,6 +1,5 @@
-
 const airports = async () => {
-    const results=await fetch('http://localhost:3333/airports')
+    const results=await fetch(`http://a66745cd4ff2e4eea89473fd030825ee-912727064.ap-southeast-1.elb.amazonaws.com/airports`)
         .then(response => response.json())
     return results
 };
